a48cf7ff110921688df2696e807f01e50cea904f
Joe Snee <snoey.92@gmail.com>
2016-08-11 06:56:43 -0500

    Python try-catches

--- bin/Device/getConnection.py
+++ bin/Device/getConnection.py
@@ -2,6 +2,9 @@ import json
 import dbus
 import sys
 bus = dbus.SystemBus()
-device_props = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0/dev_' + sys.argv[1]), "org.freedesktop.DBus.Properties")
-props = device_props.GetAll("org.bluez.Device1")
-print json.dumps(props)
+try:
+    device_props = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0/dev_' + sys.argv[1]), "org.freedesktop.DBus.Properties")
+    props = device_props.GetAll("org.bluez.Device1")
+    print json.dumps(props)
+except:
+    print ""
