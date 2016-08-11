4505e0b51f2e54109254b92bf76f38d882be6c06
Joe Snee <snoey.92@gmail.com>
2016-08-11 02:37:02 -0500

    Try not stopping CarPi on install

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo apt-get install bluez pulseaudio-module-bluetooth python-gobject python-gob
 sudo npm install pm2 -g
 cd car-pi
 npm install
-pm2 stop all
-pm2 start server.js --name="CarPi"
-sudo su -c "env PATH=$PATH:/usr/local/bin pm2 startup linux -u pi --hp /home/pi"
-pm2 save
+#pm2 stop all
+#pm2 start server.js --name="CarPi"
+#sudo su -c "env PATH=$PATH:/usr/local/bin pm2 startup linux -u pi --hp /home/pi"
+#pm2 save
 sudo reboot
